008d450d505b98eb2712b0fe0453062574c92ae4
wannabeing <wannabeing@naver.com>
2022-05-18 23:44:25 +0900

    webSocket을 이용한 간단한 채팅서비스 구현

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,41 @@
-helloo;
+const messageList = document.querySelector('ul');
+const messageForm = document.querySelector('#message');
+const nicknameForm = document.querySelector('#nickname');
+
+const socket = new WebSocket(`ws://${window.location.host}`); // Server와 연결
+
+function makeMsg(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg); // JS Object를 string으로 변환
+}
+
+// Server와 연결되었을 때
+socket.addEventListener('open', () => {
+  console.log('Connected to Server ✅');
+});
+// Server로부터 받은 메시지를 브라우저에 출력
+socket.addEventListener('message', (message) => {
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
+});
+// Server와 연결이 끊어지면 브라우저에 출력
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌');
+});
+
+// Browser의 폼 연결 및 Server에 메시지 전달
+messageForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMsg('new_msg', input.value));
+  input.value = '';
+});
+// Browser의 닉네임을 Server에 전달
+nicknameForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = nicknameForm.querySelector('input');
+  socket.send(makeMsg('nickname', input.value));
+  input.value = input.value;
+  input.disabled = true;
+});
--- src/server.js
+++ src/server.js
@@ -1,13 +1,43 @@
 import express from 'express';
+import http from 'http';
+import { parse } from 'path';
+import WebSocket from 'ws';
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public')); // user가 볼 수있는 폴더
 
-app.get('/', (req, res) => {
-  res.render('home');
-});
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 console.log('hello');
-app.listen(3000);
+
+const server = http.createServer(app); // HTTP server (Used express.js)
+const wss = new WebSocket.Server({ server }); // WebSocket server (Based on HTTP server)
+
+const sockets = [];
+
+// 브라우저와 서버를 연결한 후에 이벤트 리스너
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'anonymous'; // 기본 닉네임 설정
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from Browser ❌')); // 탭 닫았을 때 출력 (Browser와 연결 끊김)
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg); // string을 JS Object로 변환
+    switch (message.type) {
+      case 'new_msg':
+        sockets.forEach((aSocket) => {
+          aSocket.send(
+            `${socket.nickname}: ${message.payload.toString('utf-8')}`
+          );
+        });
+        break;
+      case 'nickname':
+        socket['nickname'] = message.payload;
+        break;
+    }
+  });
+});
+server.listen(3000);
